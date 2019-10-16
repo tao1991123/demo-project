0dc6ae8cc1679b0f353d16151cda9f1ca250c850
暮郴 <zhengtao.lzt@alibaba-inc.com>
2019-10-16 20:11:39 +0800

    feat: init project

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+export default () => (
+  <PageHeaderWrapper>
+    <Card>
+      <p>在此处作答</p>
+    </Card>
+  </PageHeaderWrapper>
+);
